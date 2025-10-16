75cff658247f84e920c8660d012b90e7fbd44a35
chanwinharold <chanwinharold@gmail.com>
2025-10-16 14:59:38 +0200

    🖼 Home page top section

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 
 function Sidebar() {
+    const [show, setShow] = useState(false)
     return (
-        <nav className={"absolute top-38 py-6 pl-4 pr-6 rounded-r-2xl background-connexion"}>
+        <nav onClick={() => setShow(v => !v)} className={`absolute z-20 top-38 ${show ? "max-md:-left-0" : "max-md:-left-14"} sidebar-transition py-6 pl-4 pr-6 rounded-r-2xl background-connexion`}>
             <menu className={"flex flex-col gap-y-4 items-center justify-center sidebar-menu"}>
                 <li><Link to={'/community'}><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path className={"fill-white"} d="M16 5a2.5 2.5 0 1 0 0 5a2.5 2.5 0 0 0 0-5m-4.5 2.5a4.5 4.5 0 1 1 9 0a4.5 4.5 0 0 1-9 0M6 8a2 2 0 1 0 0 4a2 2 0 0 0 0-4m-4 2a4 4 0 1 1 8 0a4 4 0 0 1-8 0m22 0a2 2 0 1 1 4 0a2 2 0 0 1-4 0m2-4a4 4 0 1 0 0 8a4 4 0 0 0 0-8M8.5 16.5q0-.334.053-.651l-4.7 1.259a2.5 2.5 0 0 0-1.767 3.062l.906 3.38a6 6 0 0 0 7.996 4.03a7.5 7.5 0 0 1-1.387-1.666a4 4 0 0 1-4.677-2.881l-.906-3.38a.5.5 0 0 1 .353-.613L8.5 17.934zm13.157 11.293a6 6 0 0 1-.646-.212a7.5 7.5 0 0 0 1.388-1.666a4 4 0 0 0 4.675-2.882l.905-3.381a.5.5 0 0 0-.353-.612L23.5 17.934V16.5q0-.332-.053-.65l4.697 1.258a2.5 2.5 0 0 1 1.767 3.062l-.906 3.38a6 6 0 0 1-7.348 4.243M12.5 14a2.5 2.5 0 0 0-2.5 2.5V22a6 6 0 0 0 12 0v-5.5a2.5 2.5 0 0 0-2.5-2.5zm-.5 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5V22a4 4 0 0 1-8 0z"/></svg></Link></li>
                 <li><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path className={"fill-white"} d="M13.5 2c0 .444-.193.843-.5 1.118V5h5a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V8a3 3 0 0 1 3-3h5V3.118A1.5 1.5 0 1 1 13.5 2M6 7a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zm-4 3H0v6h2zm20 0h2v6h-2zM9 14.5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3m6 0a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3"/></svg></li>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+
+import favoriteUrl from "../assets/icons/icon-favorite-inactive.svg"
+import playBtnUrl from "../assets/icons/icon-play-button.png"
+import plusBtnUrl from "../assets/icons/icon-plus.svg"
+
+import animeBgUrl from "/assets/images/background.jpg"
 
 function Home() {
     return (
         <main className={"text-dark-neutral-100 dark:text-light-neutral-100"}>
-            <div>Home Page</div>
-            <Link to={"/login"}>Go to login</Link>
+            <section className={`text-white relative min-sm:h-[500px] bg-cover bg-center py-16 px-28 max-md:px-8 flex flex-wrap-reverse min-md:justify-between`}>
+                <img className={"absolute inset-0 opacity-90 w-full h-full z-1 object-cover object-center"} src={animeBgUrl} alt=""/>
+
+                <article className={"z-5 max-w-2xl flex flex-col gap-y-4"}>
+                    <h1 className={"text-4xl max-sm:text-3xl font-bold uppercase my-4"}>Demon Slayer</h1>
+                    <p className={"font-semibold"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam aspernatur
+                        dicta dolor esse, ex magni maxime minima molestias nam nostrum quas soluta
+                        tempore? Ea iure neque obcaecati perferendis quod! Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Dicta doloribus, repellat?</p>
+                    <div className={"flex gap-4 flex-wrap"}>
+                        <button className={"btn-purple"}>See more <img width={24} height={24} src={`${playBtnUrl}`} alt="play button"/></button>
+                        <button className={"btn-purple"}>My list <img width={24} height={24} src={`${plusBtnUrl}`} alt="plus button"/></button>
+                        <img className={""} src={`${favoriteUrl}`} alt="a heart"/>
+                    </div>
+                    <div className={"flex items-center gap-x-4"}>
+                        <div className={"flex gap-x-2 items-center hover:scale-110 duration-300"}>
+                            <span className={"uppercase font-bold text-black text-xs bg-primary-note p-1 rounded-md"}>Note</span>
+                            <em className={"text-primary-note font-black not-italic text-lg"}>7.7</em>
+                        </div>
+                        <span className={"font-bold hover:scale-110 duration-300 border-2 rounded-md px-2 p-px"}>2018</span>
+                        <span className={"font-bold hover:scale-110 duration-300"}>Action - Samurai</span>
+                    </div>
+                </article>
+
+                <div className={"z-5 flex gap-x-4"}>
+                    <span className={"uppercase text-3xl max-sm:text-xl font-bold"}>N°</span><strong className={"text-8xl max-sm:text-6xl -mt-2"}>1</strong>
+                </div>
+            </section>
+
+            <section>
+                {/*  Code here  */}
+            </section>
         </main>
     );
 }
